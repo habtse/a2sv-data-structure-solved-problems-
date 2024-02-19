eb598ce5d337e49ea7ef9d2cb1e8cdf15e2addd9
Habtamu Tsegaye <91554252+habtse@users.noreply.github.com>
2024-02-19 10:14:32 +0300

    Add solution for Transpose Matrix

---
+++ leetcode/leetcode/transpose-matrix.py
@@ -0,0 +1,6 @@
+import numpy as np
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        mainMatrix =np.array(matrix) 
+        return mainMatrix.transpose()
+
